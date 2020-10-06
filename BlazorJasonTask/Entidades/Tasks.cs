95f0da7a9a225fa146c307b71c0d82a38b1074a0
fredchilio <fredchilio@yahoo.com.br>
2020-10-06 20:10:49 +0100

    Finishing Componets

---
+++ BlazorJasonTask/Entidades/Tasks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorJasonTask.Entidades
+{
+    public class Tasks
+    {
+        public Guid ID { get; set; }
+        public string Description { get; set; }
+        public bool Concluded { get; set; }
+        public DateTime DateCriateded { get; set; }
+    }
+}
